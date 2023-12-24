b602bddc9d289dd521fdadd29bff2d2134680d77
alexander kostiuk <alexander.i.kostiuk@outlook.com>
2023-12-24 20:28:07 -0300

    mejoras varias

--- js/index_objetos.js
+++ js/index_objetos.js
@@ -9,18 +9,18 @@ class Producto {
 }
 
 // whiskies
-const PROD1 = new Producto("Jack daniels", "./assets/whiskies/jack_daniels.jpg", "whiskies", "jack-daniels", 10000)
-const PROD2 = new Producto("Old parr 12 años", "./assets/whiskies/old_parr_12_años.jpg", "whiskies", "old-parr", 18500)
-const PROD3 = new Producto("Old parr 12 años", "./assets/whiskies/old_parr_12_años.jpg", "whiskies", "old-parr", 18500)
-const PROD4 = new Producto("Johnnie walker 18 años 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_red_label.jpg", "whiskies", "johnnie-walker", 30000)
-const PROD5 = new Producto("Johnnie Walker Red Label 1L", "/assets/whiskies/johnnie_walker/johnnie_walker_red_label.jpg", "whiskies", "johnnie-walker", 15000)
-const PROD6 = new Producto("Johnnie Walker Double Black 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_double_black.jpg", "whiskies", "johnnie-walker", 40000)
-const PROD7 = new Producto("Johnnie walker black label 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_black_label.jpg", "whiskies", "johnnie-walker", 15000)
-const PROD8 = new Producto("Johnnie walker swing 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_swing.jpg", "whiskies", "johnnie-walker", 20000)
+const W_PROD1 = new Producto("Jack daniels", "./assets/whiskies/jack_daniels.jpg", "whiskies", "jack-daniels", 10000)
+const W_PROD2 = new Producto("Old parr 12 años", "./assets/whiskies/old_parr_12_años.jpg", "whiskies", "old-parr", 18500)
+const W_PROD3 = new Producto("Johnnie walker 18 años 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_red_label.jpg", "whiskies", "johnnie-walker", 30000)
+const W_PROD4 = new Producto("Johnnie Walker Red Label 1L", "/assets/whiskies/johnnie_walker/johnnie_walker_red_label.jpg", "whiskies", "johnnie-walker", 15000)
+const W_PROD5 = new Producto("Johnnie Walker Double Black 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_double_black.jpg", "whiskies", "johnnie-walker", 40000)
+const W_PROD6 = new Producto("Johnnie walker black label 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_black_label.jpg", "whiskies", "johnnie-walker", 15000)
+const W_PROD7 = new Producto("Johnnie walker swing 750 ml", "./assets/whiskies/johnnie_walker/johnnie_walker_swing.jpg", "whiskies", "johnnie-walker", 20000)
 
-const ArrayProductos = [PROD1, PROD2, PROD3, PROD4, PROD5, PROD6, PROD7, PROD8]
+const ArrayProductos = [W_PROD1, W_PROD2, W_PROD3, W_PROD4, W_PROD5, W_PROD6, W_PROD7]
 
 const contenedor_productos = document.querySelector("#contenedor-productos");
+const botones_categorias = document.querySelectorAll(".boton-categoria");
 
 function carga_productos() {
     ArrayProductos.forEach(producto => {
@@ -38,3 +38,23 @@ function carga_productos() {
 }
 
 carga_productos()
+
+
+const btn_agregar_carrito = document.querySelectorAll(".producto-agregar");
+btn_agregar_carrito.forEach(boton => {
+        boton.addEventListener("click", () =>{
+            //aca en alert va a decir el titulo y el precio del producto seleccionado 
+            alert("Carrito 1 producto(s)\naca va ir el nombre de los productos agregados, ejemplo...\n-1 jack daniels\n-$10000")
+        })
+})
+
+
+botones_categorias.forEach(boton => {
+    boton.addEventListener("click", (evento) => {
+
+        //saco el active de todos los botones y coloco el actual al hacer click
+        botones_categorias.forEach(boton => boton.classList.remove("active"))
+        evento.currentTarget.classList.add("active")
+        //aca voy a hacer el filtro para que al hacer click me muestre en pantalla todos los productos de la categoria elegida
+    })
+})
