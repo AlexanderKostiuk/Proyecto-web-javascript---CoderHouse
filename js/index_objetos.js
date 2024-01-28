d196bdc39b2f4cf434303724865f26c4a40de5c6
alexander kostiuk <alexander.i.kostiuk@outlook.com>
2024-01-27 23:30:38 -0300

    Cambio footer y agrego sección 'aperitivos'.

--- js/index_objetos.js
+++ js/index_objetos.js
@@ -9,6 +9,7 @@ class Producto {
     }
 }
 
+
 // VINOS
 const V_PROD1 = new Producto(1, "Alma mora blend blanco 750 ml", "./assets/vinos/alma_mora_blend_blanco_750ml.jpg", "vinos", "alma-mora", 6000)
 const V_PROD2 = new Producto(2, "Alma mora malbec 750 ml", "./assets/vinos/alma_mora_malbec_750ml.jpg", "vinos", "alma-mora", 6000)
@@ -29,22 +30,31 @@ const W_PROD7 = new Producto(14, "Johnnie walker swing 750 ml", "./assets/whiski
 const C_PROD1 = new Producto(15,"Amstel Lager lata 473 ml", "./assets/cervezas/amstel_lager_lata_473ml.jpg", "cervezas", "amstel", 1000)
 const C_PROD2 = new Producto(16,"Amstel Lager lata 473 ml", "./assets/cervezas/amstel_lager_lata_710ml.jpg", "cervezas", "amstel", 1000)
 const C_PROD3 = new Producto(17,"Amstel Lager lata 473 ml", "./assets/cervezas/blue_moon_belgian_white_lata_473ml.jpg", "cervezas", "amstel", 1000)
-const C_PROD4 = new Producto(18,"Amstel Lager lata 473 ml", "./assets/cervezas/heineken_lata_473ml.jpg","cervezas", "amstel", 1000)
-const C_PROD5 = new Producto(18,"Amstel Lager lata 473 ml", "./assets/cervezas/heineken_lata_710ml.jpg", "cervezas", "amstel", 1000)
-const C_PROD6 = new Producto(19,"Iguana pilsen", "./assets/cervezas/iguana_pilsen_473ml.jpg", "cervezas", "iguana", 1000)
-const C_PROD7 = new Producto(20,"imperial golden lata 473 ml", "./assets/cervezas/imperial_golden_lata_473ml.jpg", "cervezas", "imperial", 1000)
-const C_PROD8 = new Producto(21,"imperial golden lata 710 ml", "./assets/cervezas/imperial_golden_lata_710ml.jpg", "cervezas", "imperial", 1000)
-const C_PROD9 = new Producto(22,"imperial ipa lata 473 ml", "./assets/cervezas/imperial_ipa_lata_473ml.jpg", "cervezas", "imperial", 1000)
-const C_PROD10 = new Producto(23,"imperial lager lata 473 ml", "./assets/cervezas/imperial_lager_lata_473ml.jpg", "cervezas", "imperial", 1000)
-const C_PROD11 = new Producto(24,"imperial lager lata 710 ml", "./assets/cervezas/imperial_lager_lata_710ml.jpg", "cervezas", "imperial", 1000)
-const C_PROD12 = new Producto(25,"imperial roja lata 473 ml", "./assets/cervezas/imperial_roja_lata_473ml.jpg", "cervezas", "imperial", 1000)
-//spirits
-
+const C_PROD4 = new Producto(18,"Heineken lata 473 ml", "./assets/cervezas/heineken_lata_473ml.jpg","cervezas", "heineken", 1000)
+const C_PROD5 = new Producto(19,"Heineken lata 710 ml", "./assets/cervezas/heineken_lata_710ml.jpg", "cervezas", "heineken", 1000)
+const C_PROD6 = new Producto(20,"Iguana pilsen", "./assets/cervezas/iguana_pilsen_473ml.jpg", "cervezas", "iguana", 1000)
+const C_PROD7 = new Producto(21,"imperial golden lata 473 ml", "./assets/cervezas/imperial_golden_lata_473ml.jpg", "cervezas", "imperial", 1000)
+const C_PROD8 = new Producto(22,"imperial golden lata 710 ml", "./assets/cervezas/imperial_golden_lata_710ml.jpg", "cervezas", "imperial", 1000)
+const C_PROD9 = new Producto(23,"imperial ipa lata 473 ml", "./assets/cervezas/imperial_ipa_lata_473ml.jpg", "cervezas", "imperial", 1000)
+const C_PROD10 = new Producto(24,"imperial lager lata 473 ml", "./assets/cervezas/imperial_lager_lata_473ml.jpg", "cervezas", "imperial", 1000)
+const C_PROD11 = new Producto(25,"imperial lager lata 710 ml", "./assets/cervezas/imperial_lager_lata_710ml.jpg", "cervezas", "imperial", 1000)
+const C_PROD12 = new Producto(26,"imperial roja lata 473 ml", "./assets/cervezas/imperial_roja_lata_473ml.jpg", "cervezas", "imperial", 1000)
+//aperitivos
+const A_PROD1 = new Producto(25,"cinzano rosso 1l", "./assets/aperitivos/cinzano_rosso_1l.jpg", "aperitivos", "cinzano", 1000)
+const A_PROD2 = new Producto(25,"cinzano rosso 450 ml", "./assets/aperitivos/cinzano_rosso_450ml.jpg", "aperitivos", "cinzano", 1000)
+const A_PROD3 = new Producto(25,"fernet branca 1 lt", "./assets/aperitivos/fernet_branca_1lt.jpg", "aperitivos", "fernet", 1000)
+const A_PROD4 = new Producto(25,"fernet branca 750 ml", "./assets/aperitivos/fernet_branca_750ml.jpg", "aperitivos", "fernet", 1000)
+const A_PROD5 = new Producto(25,"fernet buhero negro 700 ml", "./assets/aperitivos/fernet_buhero_negro_700ml.jpg", "aperitivos", "fernet", 1000)
+const A_PROD6 = new Producto(25,"hesperidina", "./assets/aperitivos/hesperidina_1lt.jpg", "aperitivos", "hesperidina", 1000)
+const A_PROD7 = new Producto(25,"martini bianco 1lt", "./assets/aperitivos/martini_bianco_1lt.jpg", "aperitivos", "martini", 1000)
+const A_PROD8 = new Producto(25,"martini extra dry 950 ml", "./assets/aperitivos/martini_extra_dry_950ml.jpg", "aperitivos", "martini", 1000)
 
 const ArrayProductos = [V_PROD1, V_PROD2, V_PROD3, V_PROD4, V_PROD5, V_PROD6, V_PROD7,
                         W_PROD1, W_PROD2, W_PROD3, W_PROD4, W_PROD5, W_PROD6, W_PROD7,
                         C_PROD1, C_PROD2, C_PROD3, C_PROD4, C_PROD5, C_PROD6, C_PROD7,
-                        C_PROD8, C_PROD9, C_PROD10, C_PROD11, C_PROD12
+                        C_PROD8, C_PROD9, C_PROD10, C_PROD11, C_PROD12,
+                        A_PROD1, A_PROD2, A_PROD3, A_PROD4, A_PROD5, A_PROD6, A_PROD7,
+                        A_PROD8,
                        ]
 
 
